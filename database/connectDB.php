0bc8ad1777fdfd20c72add0325aa93ddd3f39e2a
Ferney Jerez <ferney9212@gmail.com>
2018-11-15 22:05:35 -0500

    connect datafase for mysql

---
+++ database/connectDB.php
@@ -0,0 +1,7 @@
+<?php
+    $dbServername="localhost";
+    $dbServername="root";
+    $dbPassword="root";
+    $dbName="test";
+    $conn=mysqli_connect($dbServername, $dbServername, $dbPassword, $dbName);
+?>
